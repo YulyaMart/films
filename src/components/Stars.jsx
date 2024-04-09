b76cebab35cfb20687750addfb77c54ad22add31
YlyaMart <yulya.martinova94@gmail.com>
2024-04-09 13:06:39 +0300

    init commit

---
+++ src/components/Stars.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import Star from './Star';
+
+export default function Stars ({count}) {
+    if (typeof count !== 'number' || isNaN(count) || count <1 || count >5) {
+      return null;
+    }
+    return (
+      <ul className="card-body-stars u-clearfix">
+        {Array(count).fill().map((_, count) => 
+        <li key={count}><Star /></li>
+        )}
+      </ul>
+    )
+  }
+  
+  Stars.propTypes = {
+    count: PropTypes.number.isRequired
+  }
+  
